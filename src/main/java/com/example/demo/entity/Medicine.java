69eeffbbd0b5c67ea9abbf8733f0b2fe317b0366
DivyaIpar <divyaipar08@gmail.com>
2025-07-10 18:42:12 +0530

    commit 2

--- src/main/java/com/example/demo/entity/Medicine.java
+++ src/main/java/com/example/demo/entity/Medicine.java
@@ -4,4 +4,23 @@ public class Medicine {
 	int NoOfmedicine;
 	String Name;
 	int  Price;
+	public int getNoOfmedicine() {
+		return NoOfmedicine;
+	}
+	public void setNoOfmedicine(int noOfmedicine) {
+		NoOfmedicine = noOfmedicine;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getPrice() {
+		return Price;
+	}
+	public void setPrice(int price) {
+		Price = price;
+	}
+	
 }
